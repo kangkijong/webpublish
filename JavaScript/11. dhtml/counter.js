3015f5e002bd52b5c5ec61ee11d43200e9b8cc0d
kang <kijong4309@naver.com>
2025-09-02 16:18:55 +0900

    2025-09-02 commit

---
+++ JavaScript/11. dhtml/counter.js
@@ -0,0 +1,37 @@
+// 숫자 증가/감소 함수
+function counter(type) {
+    let number = document.querySelector('#number').textContent;
+
+    if(type === 'increment') {
+        if(number >= 0) {
+            document.querySelector('#number').textContent = ++number;
+        }
+    } else {
+        if(number > 0) {
+            document.querySelector('#number').textContent = --number;
+        }
+    }
+}
+
+// 숫자 증가 함수
+function increment() {
+    // alert('increment');
+    let number = document.querySelector('#number').textContent;   // form -> input
+    
+    // 0보다 크면 1증가
+    if(number >= 0) {
+        document.querySelector('#number').textContent = ++number;
+    }
+    
+}
+
+// 숫자 감소 함수
+function decrement() {
+    // alert('decrement');
+    let number = document.querySelector('#number').textContent;
+
+    // 0보다 크면 1감소
+    if(number > 0) {
+        document.querySelector('#number').textContent = --number;
+    }
+}
