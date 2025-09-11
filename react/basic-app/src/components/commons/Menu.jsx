dd3e477097679f077b71907816c3fdd7af8e86a0
kang <kijong4309@naver.com>
2025-09-11 16:48:06 +0900

    2025-09-11 commit

---
+++ react/basic-app/src/components/commons/Menu.jsx
@@ -0,0 +1,9 @@
+export function Menu({href, name, style, isIcon, icon}) {
+    return (
+        <a  href={href} 
+            className="menu-item" 
+            style={{width: style.w, height: style.h, backgroundColor: style.bg, color: style.color}}>
+            {isIcon ? icon : ""} {name}
+        </a>
+    );
+}
