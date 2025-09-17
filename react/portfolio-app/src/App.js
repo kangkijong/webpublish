937ebd8fb66a5b5eca168e421929e8050faaaee8
kang <kijong4309@naver.com>
2025-09-17 17:55:55 +0900

    2025-09-17 commit

--- react/portfolio-app/src/App.js
+++ react/portfolio-app/src/App.js
@@ -26,14 +26,16 @@ export default function App() {
   };
   const [data, setData] = useState(init);
 
-  useEffect(() => {
-    const load = async () => {
-      const response = await fetch("/data/portfolio.json");
-      const jsonData = await response.json(); // json 파싱 작업
-      setData(jsonData);
+  useEffect(() => { // 컴포넌트가 최초로 화면에 렌더링(mount) 될 때만 실행됨
+    const load = async () => {  // 비동기 함수 정의 (데이터를 가져오는 함수)
+      const response = await fetch("/data/portfolio.json"); // JSON 파일을 비동기로 요청
+      const jsonData = await response.json(); // JSON 형식으로 파싱
+      setData(jsonData);  // 받아온 데이터를 상태로 저장 (컴포넌트 리렌더링 발생)
     }
-    load();
-  }, []);
+    load(); // 정의한 비동기 함수 실행
+  }, []); // []는 빈 배열이기 때문에 useEffect()는 딱 한 번, 컴포넌트가 마운트될 때만 실행
+          // 즉, 이 fetch() 요청은 처음에 한 번만 실행
+          // 만약 배열에 어떤 상태가 들어가 있다면 그 상태가 바뀔 때마다 실행
 
   // console.log('majors --->', data.content.about.majors);
 
