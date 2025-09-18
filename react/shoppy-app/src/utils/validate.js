43aa96c07eb214e67bc20d9f005e219105971be8
kang <kijong4309@naver.com>
2025-09-18 16:57:07 +0900

    2025-09-18 commit

---
+++ react/shoppy-app/src/utils/validate.js
@@ -0,0 +1,34 @@
+/**
+ * Shoppy 로그인 폼 체크
+ */
+export const validateFormCheck = ({idRef, pwdRef, setErrors, errors}) => {
+    if(idRef.current.value === "") {
+        // alert("아이디를 입력해주세요");
+        setErrors({...errors, id: "아이디를 입력해주세요"});
+        idRef.current.focus();
+        return false;
+    } else if(pwdRef.current.value === "") {        // 패스워드 입력 검사
+        // alert("패스워드를 입력해주세요");
+        setErrors({...errors, pwd: "패스워드를 입력해주세요"});
+        pwdRef.current.focus();
+        return false;
+    } 
+    return true;
+}
+
+/**
+ * 로그인 폼 체크
+ */
+export function validateLoginCheck(refs, setMsg) {
+    
+    if(refs.idRef.current.value === "") {
+        setMsg({id: "아이디를 입력해주세요"});
+        refs.idRef.current.focus();
+        return false;
+    } else if(refs.passRef.current.value === "") {
+        setMsg({pass: "패스워드를 입력해주세요"});
+        refs.passRef.current.focus();
+        return false;
+    } 
+    return true;
+}
