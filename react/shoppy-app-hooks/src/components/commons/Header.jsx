6a04013c3018bc11d69784556ba68a4f8152c5ec
kang <kijong4309@naver.com>
2025-09-25 11:27:20 +0900

    2025-09-25 commit

---
+++ react/shoppy-app-hooks/src/components/commons/Header.jsx
@@ -0,0 +1,36 @@
+import { useState, useEffect, useContext } from 'react';    //
+import { Link } from 'react-router-dom';
+import { FiShoppingBag } from "react-icons/fi";
+import { GiShoppingCart } from "react-icons/gi";
+import { CartContext } from '../../context/CartContext';    //
+
+export function Header() {                                  //
+    const  {cartCount} = useContext(CartContext);           //
+
+    return (
+        <div className="header-outer">
+            <div className="header">
+                <Link to="/" className='header-left'>
+                    <FiShoppingBag />
+                    <span>Shoppy-hooks</span>
+                </Link>
+                <nav className='header-right'>
+                    <Link to="/all">Products</Link>
+                    <Link to="/cart" className="header-icons-cart-link">
+                        <GiShoppingCart className='header-icons'/>
+                        <span className='header-icons-cart'>{cartCount}</span>
+                    </Link>
+                    <Link to="/login">
+                        <button type="button">Login</button>
+                    </Link>
+                    <Link to="/signup">
+                        <button type="button">Signup</button>
+                    </Link>                   
+                    <Link to="/support">
+                        <button type="button">Support</button>
+                    </Link>                   
+                </nav>
+            </div>
+        </div>
+    );
+}
